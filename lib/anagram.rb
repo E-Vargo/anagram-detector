c29e303e601f138aba37b531da3797348f728936
E-Vargo <vargoelliott@gmail.com>
2021-12-02 14:21:45 -0800

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+attr_accessor :word, :array
+
+def initialize(word)
+@word = word
+end
+
+def match(array)
+@array = array
+letter_array = @word.split("").sort
+@array.select {|e| e.split("").sort == letter_array}
+end
+
+
+end
